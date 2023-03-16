2e828416033c7038b57449ea3048633ab378d173
mtlthu <mtlthu1991@gmail.com>
2023-03-16 17:44:01 +0700

    test routers

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from '~/components/GlobalStyle';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <GlobalStyle>
-            <BrowserRouter>
+        <HashRouter>
+            <GlobalStyle>
+                {/* <BrowserRouter> */}
                 <App />
-            </BrowserRouter>
-        </GlobalStyle>
+                {/* </BrowserRouter> */}
+            </GlobalStyle>
+        </HashRouter>
     </React.StrictMode>,
 );
 
