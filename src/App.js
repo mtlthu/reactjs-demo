94cd5804bd655e380530df80ccd05991c43dc117
mtlthu <mtlthu1991@gmail.com>
2023-03-16 17:03:49 +0700

    test route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+// import { BrowserRouter as Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/reactjs-demo/" element={<Home />} />
-                    <Route path="/reactjs-demo/following" element={<Following />} />
-                </Routes>
-            </div>
-        </Router>
+        <div className="app">
+            <Routes>
+                <Route exact path="/reactjs-demo" element={<Home />} />
+                <Route path="/reactjs-demo/following" element={<Following />} />
+            </Routes>
+        </div>
+        // <HashRouter>
+        //     <Routes>
+        //         <Route path="/reactjs-demo/" element={<Home />} />
+        //         <Route path="/reactjs-demo/following" element={<Following />} />
+        //     </Routes>
+        // </HashRouter>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from '~/components/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <GlobalStyle>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </GlobalStyle>
     </React.StrictMode>,
 );
