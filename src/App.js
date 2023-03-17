258ee4d1751fe08ff9bf7d47f9cef884fb9081d4
mtlthu <mtlthu1991@gmail.com>
2023-03-17 11:36:27 +0700

    create layout tiktok page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 
 function App() {
     return (
         <Router>
             <div className="app">
                 <Routes>
-                    {/* <Route exact path="/reactjs-demo" element={<Home />} />
-                <Route path="/reactjs-demo/following" element={<Following />} /> */}
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
