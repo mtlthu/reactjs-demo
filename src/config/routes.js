62debeb0437af69fab93ff5f64eb751a71fe9191
mtlthu <mtlthu1991@gmail.com>
2023-03-18 14:48:34 +0700

    create tiktok UI

---
+++ src/config/routes.js
@@ -0,0 +1,9 @@
+const routes = {
+    home: 'reactjs-demo/',
+    following: 'reactjs-demo/following',
+    profile: 'reactjs-demo/@:nickname',
+    upload: 'reactjs-demo/upload',
+    search: 'reactjs-demo/search',
+    live: 'reactjs-demo/live',
+};
+export default routes;
